6421a8179e28ba49500c5abf0034114c459110bd
Alexiscnl <alexis.cornillon@gmail.com>
2025-06-17 09:38:17 +0200

    feat: implement function user

---
+++ part2/hbnb/app/models/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from base_model import BaseModel
+
+class User(BaseModel):
+
+    def __init__(self, first_name, last_name, email, is_admin=False):
+        super().__init__()
+
+        if len(first_name) > 50:
+            raise ValueError("Your first name must have less than 50 character")
+        else:
+            self.first_name = first_name
+
+        if len(last_name) > 50:
+            raise ValueError("Your last name must have less than 50 character")
+        else:
+            self.last_name = last_name
+
+        if not '@' in email:
+            raise TypeError("enter a valid adress ex: exemple@gmail.com")
+        else:
+            self.email = email
+
+        self.is_admin = is_admin
